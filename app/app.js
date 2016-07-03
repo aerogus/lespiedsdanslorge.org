e9ec28ce568228cc39055aa77413ddc7b5052c2e
Guillaume Seznec <guillaume.seznec@ouifm.fr>
2016-07-03 18:11:32 +0200

    suite derfest

--- app/app.js
+++ app/app.js
@@ -1,9 +1,66 @@
 "use strict";
 
 var App = {
+
+  route: '',
+
+  UI: {
+    main: document.getElementById('main-content')
+  },
+
   init: function () {
+    var _this = this;
     console.log('App.init');
+    this.route = window.location.pathname;
+    console.log(this.route);
+    this.UI.main.innerHTML = this.get_page(this.route);
+
+    // intercepte les clicks sur les liens
+    document.addEventListener('click', function (e) {
+      var e = window.e || e;
+      if (e.target.tagName !== 'A') {
+        return;
+      }
+      e.preventDefault();
+      console.log('click to ' + e.target.href);
+      window.history.pushState('', '', e.target.href);
+      _this.UI.main.innerHTML = _this.get_page(e.target.href);
+    }, false);
+
+  },
+
+  /**
+   * @param string name
+   * @return string
+   */
+  get_page: function (name) {
+    name.replace('http://localhost:3333', '');
+    console.log('get_page ' + name);
+    switch (name) {
+      case '/':
+        return '<p>coucou accueil</p>';
+        break;
+      case '/artistes/polarpolarpolarpolar':
+        var artiste = require('artistes/polarpolarpolarpolar');
+        return '<h2>' + artiste.name + '</h2>' + '<p>' + artiste.description.replace('\n', '<br>') + '</p>';
+        break;
+      case '/artistes/lesdyvettesdenface':
+        var artiste = require('artistes/lesdyvettesdenface');
+        console.log('require ' + artiste.name);
+        return '<p>coucou dyvettes</p>';
+        break;
+      case '/artistes':
+        return 'artistes';
+        break;
+      case '/partenaires':
+        return '<p>Les partenaires</p>';
+        break;
+      default:
+        return '<p>unknown page</p>';
+        break;
+    }
   }
+
 };
 
 module.exports = App;
---
+++ app/artistes/polarpolarpolarpolar.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Polar Polar Polar Polar",
+  url: "http://www.polarpolarpolarpolar.com",
+  facebook: "https://www.facebook.com/polarpolarpolarpolar",
+  description: `Polar Polar Polar Polar se forme en 2010, au fond d'une brocante de Seine-et-Oise, autour d'un carton de vinyles neufs et d'occasion.
+Energie brute, organique et intensité sonore jubilatoire, tantôt secoué de rythmiques entêtantes et agité de spasmes, tantôt immobile et suspendu pour quelques instants de recueillement, le groupe affirme son goût prononcé pour le contraste.
+Sur scène, en sueur et en transe, le quatuor propose de se laisser emporter dans une danse frénétique, sourires aux lèvres.
+Une musique progressante, à facettes, patiemment cultivée au fil des saisons, dans 4 EP de 4 titres, réalisés en 4 ans et marqueurs de l'évolution du groupe.`
+}
