07839f972951b77f842dfd542275e85e523dfb5c
Guillaume Seznec <guillaume.seznec@ouifm.fr>
2016-08-20 20:39:36 +0200

    suite

--- app/app.js
+++ app/app.js
@@ -2,27 +2,20 @@
 
 var App = {
 
-  GMAP_KEY: 'AIzaSyBW8wt3QH0k1e_oV9ue_jE8-5AOUX9OnOY',
+  gmap: {
+    api_key: 'AIzaSyBW8wt3QH0k1e_oV9ue_jE8-5AOUX9OnOY',
+    callback: 'app.init_gmap'
+  },
 
   init: function () {
-    console.log('App.init');
 
     $(document).foundation();
 
-    var _this = this;
-
     this.init_artistes();
-    this.init_artiste();
+    this.init_fiches_artistes();
     this.init_planning();
-    //this.init_grande_scene();
-    //this.init_petite_scene();
     this.init_map();
-
-    // smooth scrollTo
-    $('a[href^="#"]').click(function (e) {
-      e.preventDefault();
-      $('html,body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 'fast');
-    });
+    this.init_smooth_scroll();
 
   },
 
@@ -55,7 +48,7 @@ var App = {
   },
 
   /**
-   *
+   * Tableau des horaires
    */
   init_planning: function () {
     var artistes = require('artistes');
@@ -90,93 +83,36 @@ var App = {
   },
 
   /**
-   * Charge la section Grande Scène
-   */
-  init_grande_scene: function () {
-    var artistes = require('artistes');
-
-    // filtre grande scène
-    artistes = artistes.filter(function (obj) {
-      return obj.scene === 'grande';
-    });
-
-    // tri chronologique
-    artistes = artistes.sort(function (a, b) {
-      return a.horaire > b.horaire;
-    });
-
-    var list = $('<div class="row"/>');
-    artistes.forEach(function (e) {
-      list.append('<div class="small-6 medium-4 columns artiste">\
-        <a data-open="' + e.id + '">\
-          <img src="' + e.photo + '"/>\
-          <h4 class="button">' + e.name + ' ' + e.horaire + '</h4>\
-        </a>');
-    });
-
-    $('#grande-scene-content').append(list);
-  },
-
-  /**
-   * Charge la section Petite Scène
-   */
-  init_petite_scene: function () {
-    var artistes = require('artistes');
-
-    // filtre petite scène
-    artistes = artistes.filter(function (obj) {
-      return obj.scene === 'petite';
-    });
-
-    // tri chronologique
-    artistes = artistes.sort(function (a, b) {
-      return a.horaire > b.horaire;
-    });
-
-    var list = $('<div class="row"/>');
-    artistes.forEach(function (e) {
-      list.append('<div class="small-6 medium-4 columns artiste">\
-        <a data-open="' + e.id + '">\
-          <img src="' + e.photo + '"/>\
-          <p><strong>' + e.horaire + '</strong></p>\
-          <h4 class="button">' + e.name + '</h4>\
-        </a>');
-    });
-
-    $('#petite-scene-content').append(list);
-  },
-
-  /**
-   *
+   * insertion du script Google Maps
    */
   init_map: function () {
     var s = document.createElement('script');
-    s.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.GMAP_KEY + '&callback=app.init_gmap';
+    s.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.gmap.api_key + '&callback=' + this.gmap.callback;
     document.head.appendChild(s);
   },
 
   /**
-   *
+   * Initialisation de la Google Map
    */
   init_gmap: function () {
-    console.log('init gmap');
+    var parking = new google.maps.LatLng(48.66943944555655, 2.3234067073791653);
     var options = {
       zoom: 16,
-      center: new google.maps.LatLng(48.66943944555655, 2.3234067073791653),
+      center: parking,
       mapTypeId: google.maps.MapTypeId.HYBRID,
       scrollwheel: false
     };
     var map = new google.maps.Map(document.getElementById('map'), options);
     var marker = new google.maps.Marker({
       map: map,
-      position: new google.maps.LatLng(48.66943944555655,2.3234067073791653)
+      position: parking
     });
   },
 
   /**
-   * fiches artistes
+   * génération des fiches artistes
    */
-  init_artiste: function () {
+  init_fiches_artistes: function () {
     var artistes = require('artistes');
     artistes.forEach(function (obj) {
       var content = '<div id="' + obj.id + '" class="large reveal" data-reveal>\
@@ -194,6 +130,17 @@ var App = {
         var reveal = new Foundation.Reveal($('#' + obj.id));
     });
 
+  },
+
+  /**
+   * gestion du scrolling doux
+   */
+  init_smooth_scroll: function () {
+    // lien vers les ancres internes
+    $('a[href^="#"]').click(function (e) {
+      e.preventDefault();
+      $('html,body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 'fast');
+    });
   }
 
 };
