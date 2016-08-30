aff571c4ab053d027a3bbcd285ddfc35d00661d4
Guillaume Seznec <guillaume.seznec@ouifm.fr>
2016-08-30 18:53:44 +0200

    maj artistes

--- app/artistes.js
+++ app/artistes.js
@@ -9,10 +9,8 @@ module.exports = [
     horaire: '',
     url: "http://www.lesdyvettesdenface.fr/",
     facebook: "https://www.facebook.com/LesDyvettesDenFace/",
-    description: `"Les Dyvettes d'en Face" est un groupe vocal de 4 chanteuses à l'univers Jazzy originaire de l'Essonne.
-D’abord appelées les P’tits bouts d’femmes, elles deviennent les Dyvettes d’en face en mars 2013 et spécialisent leur répertoire vers un son polyphonique généralement a capella.
-Le groupe mèle compositions et reprises avec des arrangements originaux propres à leur univers.
-Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et leur tour de chant "Au tour du monde" (2013), Les Dyvettes reviennent avec un nouveau projet. Leur objectif: faire revivre le jazz des années 50 à travers leur univers polyphonique a capella, le tout mis en scène afin de mêler moments d'humour, d'émotion, de plaisir et de swing"!`
+    twitter: "",
+    description: ``
   },
   {
     id: 'lesdyvettesdenface',
@@ -20,15 +18,12 @@ Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et le
     style: "Quatuor vocal",
     photo: '/img/artistes/lesdyvettesdenface.jpg',
     soundcloud: '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/252147507&amp;color=0066cc"></iframe>',
-    video: '',
+    video: 'https://www.youtube.com/watch?v=mtZRQkkEntg',
     scene: 'petite',
     horaire: '20:00',
     url: "http://www.lesdyvettesdenface.fr/",
     facebook: "https://www.facebook.com/LesDyvettesDenFace/",
-    description: `"Les Dyvettes d'en Face" est un groupe vocal de 4 chanteuses à l'univers Jazzy originaire de l'Essonne.
-D’abord appelées les P’tits bouts d’femmes, elles deviennent les Dyvettes d’en face en mars 2013 et spécialisent leur répertoire vers un son polyphonique généralement a capella.
-Le groupe mèle compositions et reprises avec des arrangements originaux propres à leur univers.
-Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et leur tour de chant "Au tour du monde" (2013), Les Dyvettes reviennent avec un nouveau projet. Leur objectif: faire revivre le jazz des années 50 à travers leur univers polyphonique a capella, le tout mis en scène afin de mêler moments d'humour, d'émotion, de plaisir et de swing"!`
+    description: `Les Dyvettes d'en face est un groupe vocal de quatre chanteuses et amies d'enfance, originaires la vallée de l'Yvette (91). Elles ont sorti début 2016 un premier EP "Les Dyvettes d'en Face et leurs amis" sur lequel elles proposent leurs compositions en featuring avec des instrumentistes. C'est en live que vous pouvez découvrir ces morceaux dans leur version originale ! Les Dyvettes vous proposent un spectacle de chant a capella aux couleurs jazz, mêlant avec humour et élégance reprises et compositions, dans une mise en scène pétillante, pleine d'émotions, de plaisir et de swing!`
   },
   {
     id: 'mayavibes',
@@ -41,6 +36,7 @@ Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et le
     horaire: '22:25',
     url: "http://maya-vibes.com/",
     facebook: "https://www.facebook.com/Mayavibes", // maya.vibes.71, ProfessorX.ManagerMayaVibes
+    twitter: "https://twitter.com/MayaVibes",
     description: `En 2011, "Maya" se fait repérer par une équipe de musiciens qui lui propose de travailler ensemble. En 2012, ils sortent leur premier titre intitulé Sweety, un mélange audacieux de musiques latines sur fond de reggae ragga. Aujourd’hui, ils collaborent toujours ensemble et ont construit une véritable équipe d’artistes, musiciens, vidéastes autour d’un projet commun : la production du premier album de Maya Vibes.
   En 2013 sort «HITS OK», la seconde mixtape de Maya, sur laquelle il pose ses textes sur des instrumentales hip hop et reggae. Il y invite des artistes de l’Essonne et de la région parisienne tels que Poizon, Scars, Taly ou encore Volodia du collectif Phases Cachées.`
   },
@@ -49,11 +45,12 @@ Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et le
     name: "Odd Fiction",
     style: 'Métal Progressif',
     photo: '/img/artistes/oddfiction.jpg',
-    video: '',
+    video: 'https://www.youtube.com/watch?v=cgpRxv-xJA8',
     scene: 'grande',
     horaire: '19:30',
     url: "",
     facebook: "https://www.facebook.com/OddFiction/",
+    twitter: "",
     description: `De la grosse disto, une touche de post rock, une pointe de jazz, Odd Fiction est un groupe de métal progressif instrumental qui brise les frontières de la musique en alliant brutal et finesse, créant une fiction pour le moins étrange. Cette bête hideuse, hybride musical, créé des atmosphères tantôt violentes, tantôt planante, parfois clownesque, installant dans l'espace sonore un non sens absurde, bien vite rattrapé par des grooves métal à s'en décrocher la tête. Mais ne fuyez pas l'antre de la bête, car c'est à l'intérieur qu'on y entend la douce musique de ses cris lugubres...`
   },
   {
@@ -61,11 +58,12 @@ Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et le
     name: "PandraVox",
     style: 'Trip hop Rock Alternative',
     photo: '/img/artistes/pandravox.jpg',
-    video: '',
+    video: 'https://www.youtube.com/watch?v=eM6DfcfxWvw',
     scene: 'petite',
     horaire: '21:00',
-    url: "https://itunes.apple.com/fr/artist/pandra-vox/id1047657229",
+    url: "http://www.pandravox.com/",
     facebook: "https://www.facebook.com/pandravoxetc/",
+    twitter: "https://twitter.com/pandravox",
     description: `Portés par les textes et la voix puissante et cristalline de leur chanteuse, les 4 musiciens de Pandra Vox conjuguent dans leur cabinet de curiosités trip hop, électro organique, parcelles jazzy et énergie rock.`
   },
   {
@@ -78,6 +76,7 @@ Après leur spectacle "Les p'tits bouts d'femmes font leur cabaret" (2011) et le
     horaire: '23:05',
     url: "http://www.polarpolarpolarpolar.com",
     facebook: "https://www.facebook.com/polarpolarpolarpolar",
+    twitter: "https://twitter.com/PolarX4",
     description: `Polar Polar Polar Polar se forme en 2010, au fond d'une brocante de Seine-et-Oise, autour d'un carton de vinyles neufs et d'occasion.
 Energie brute, organique et intensité sonore jubilatoire, tantôt secoué de rythmiques entêtantes et agité de spasmes, tantôt immobile et suspendu pour quelques instants de recueillement, le groupe affirme son goût prononcé pour le contraste.
 Sur scène, en sueur et en transe, le quatuor propose de se laisser emporter dans une danse frénétique, sourires aux lèvres.
@@ -94,6 +93,7 @@ Une musique progressante, à facettes, patiemment cultivée au fil des saisons, 
     url: "http://pricklypearl.wix.com/website",
     soundclound: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/6455041&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false",
     facebook: "https://www.facebook.com/PricklyPearl/",
+    twitter: "https://twitter.com/Prickly_Pearl",
     description: `Le Projet Prickly Pearl débute en 2011. Loïc, après diverses expériences, veut revenir à ses premières amours aux sources de la musique populaire américaine.
 Sa rencontre avec Carole, à la voix grave et aux accents "Soul", est comme une évidence. Ensemble, ils vont ciseler et modeler les morceaux et définir les bases de la musique de Prickly Pearl: une Folk hybride aux racines multiples Country, Jazz, Blues, Soul.
 Dès leurs premiers passages dans les clubs, ils rencontrent un public enthousiaste, qui les encourage à poursuivre, et leur fait prendre conscience de l'aspect fédérateur de leur musique. Puis les salles de concert s’agrandissent : le Sentier des Halles et le Batofar.
@@ -127,6 +127,7 @@ La sortie du premier EP 6 titres de Prickly Pearl est prévu pour début 2017.`
     horaire: '20:30',
     url: "",
     facebook: "https://www.facebook.com/smokeybanditsband/",
+    twitter: "",
     description: `Smokey Bandits c'est avant tout du rock ! Un groupe énergique, groovy aux influences multiples !`
   },
   {
@@ -139,6 +140,7 @@ La sortie du premier EP 6 titres de Prickly Pearl est prévu pour début 2017.`
     horaire: '18:30',
     url: "http://www.so-zik.fr/",
     facebook: "https://www.facebook.com/sozik91/",
+    twitter: "",
     description: `"SOZIK" propose un enseignement musical inédit, axé sur la création. Venez apprendre un instrument, à composer et à enregistrer vos propres morceaux !`
   }
 ];
