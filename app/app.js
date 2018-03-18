b1fe65f65f5daa7b12e58839be9fd6e4981c04f4
Guillaume Seznec <guillaume.seznec@gmail.com>
2018-03-18 18:59:35 +0100

    integ du flyer, fix bug modal

--- app/app.js
+++ app/app.js
@@ -11,14 +11,44 @@ const gmap =
   lng: 2.3234067073791653,
 };
 
-/**
- * @param {string} id
- */
-function showModal(id) {
-  if (!id) return;
-  $('.modal').show(); // affiche le fond
-  $('.modal-content').hide(); // ferme les autres
-  $(`#${id}`).show();
+function buildModal(obj) {
+  let content = `<div class="clearfix modal-content" id="${obj.id}">
+  <div class="bfc">
+  <img src="${obj.photo}" width="480" height="360" style="float:left;padding-right:10px;padding-bottom:10px">
+  <h4>${obj.name}</h4>
+  <p><strong>${obj.style}</strong></p>
+  <p>Scène: <strong>${obj.scene}</strong></p>`;
+
+  if (obj.links.site) {
+    content += `<a class="badge social site" href="${obj.links.site}" target="_blank" title="lien vers le site"><img src="/img/social/site.svg" alt=""/></a>`;
+  }
+  if (obj.links.facebook) {
+    content += `<a class="badge social facebook" href="${obj.links.facebook}" target="_blank" title="lien vers le Facebook"><img src="/img/social/facebook.svg"/></a>`;
+  }
+  if (obj.links.twitter) {
+    content += `<a class="badge social twitter" href="${obj.links.twitter}" target="_blank" title="lien vers le Twitter"><img src="/img/social/twitter.svg"/></a>`;
+  }
+  if (obj.links.youtube) {
+    content += `<a class="badge social youtube" href="${obj.links.youtube}" target="_blank" title="lien vers le YouTube"><img src="/img/social/youtube.svg"/></a>`;
+  }
+  if (obj.links.instagram) {
+    content += `<a class="badge social instagram" href="${obj.links.instagram}" target="_blank" title="lien vers le Instagram"><img src="/img/social/instagram.svg"/></a>`;
+  }
+  if (obj.links.bandcamp) {
+    content += `<a class="badge social bandcamp" href="${obj.links.bandcamp}" target="_blank" title="lien vers le bandcamp"><img src="/img/social/bandcamp.svg"/></a>`;
+  }
+
+  content += `<p>${obj.description.replace('\n', '<br>')}</p></div>
+    <button class="close-button">X</button>`;
+
+  if (obj.video) {
+    content += `<div class="fluid-video">
+      <iframe width="1280" height="720" src="${obj.video}" frameborder="0" allowfullscreen></iframe>
+    </div>`;
+  }
+
+  content += '</div>';
+  $('#artiste-content').append(content);
 }
 
 /**
@@ -26,7 +56,32 @@ function showModal(id) {
  */
 function hideModal(id) {
   if (id) $(`#${id}`).hide();
+  $('.modal').empty();
   $('.modal').hide();
+}
+
+function handleModalControls() {
+  // echap ferme la modale
+  $(document).keyup((e) => { if (e.keyCode === 27) hideModal(); });
+
+  // click à l'extérieur ferme la modale
+  $('.modal').click((e) => { if (e.target.id === 'artiste-content') hideModal(); });
+
+  // click bouton X
+  $('.close-button').click(e => hideModal(e.currentTarget.parentNode.id));
+}
+
+/**
+ * @param {string} id
+ */
+function showModal(id) {
+  if (!id) return;
+  const artiste = dbArtistes.filter(obj => (obj.id === id))[0];
+  buildModal(artiste);
+  handleModalControls();
+  $('.modal').show(); // affiche le fond
+  $('.modal-content').hide(); // ferme les autres
+  $(`#${id}`).show();
 }
 
 function initResponsiveMenu() {
@@ -70,32 +125,6 @@ function initArtistes() {
 }
 
 /**
- * Tableau des horaires
- */
-function initPlanning() {
-  // filtre grande scène ou petite scène
-  const artistes = dbArtistes.filter(obj => obj.scene === 'grande' || obj.scene === 'petite');
-  // tri chronologique
-  artistes.sort((a, b) => a.horaire > b.horaire);
-  const table = $('#planning-table');
-  const tbody = $('<tbody/>');
-  artistes.forEach((artiste) => {
-    const tr = $('<tr/>');
-    tr.append($('<th/>', {
-      text: artiste.horaire,
-    }))
-      .append($('<td/>', {
-        html: artiste.scene === 'petite' ? `<a data-open="${artiste.id}">${artiste.name}</a>` : '',
-      }))
-      .append($('<td/>', {
-        html: artiste.scene === 'grande' ? `<a data-open="${artiste.id}">${artiste.name}</a>` : '',
-      }));
-    tbody.append(tr);
-  });
-  table.append(tbody);
-}
-
-/**
  * insertion du script Google Maps
  */
 function initMap() {
@@ -123,61 +152,6 @@ function gmapCallback() {
 }
 
 /**
- * génération des fiches artistes
- */
-function initFichesArtistes() {
-  dbArtistes.forEach((obj) => {
-    let content = `<div class="clearfix modal-content" id="${obj.id}">
-      <div class="bfc">
-      <img src="${obj.photo}" width="480" height="360" style="float:left;padding-right:10px;padding-bottom:10px">
-      <h4>${obj.name}</h4>
-      <p><strong>${obj.style}</strong></p>
-      <p>Scène: <strong>${obj.scene}</strong></p>`;
-
-    if (obj.links.site) {
-      content += `<a class="badge social site" href="${obj.links.site}" target="_blank" title="lien vers le site"><img src="/img/social/site.svg" alt=""/></a>`;
-    }
-    if (obj.links.facebook) {
-      content += `<a class="badge social facebook" href="${obj.links.facebook}" target="_blank" title="lien vers le Facebook"><img src="/img/social/facebook.svg"/></a>`;
-    }
-    if (obj.links.twitter) {
-      content += `<a class="badge social twitter" href="${obj.links.twitter}" target="_blank" title="lien vers le Twitter"><img src="/img/social/twitter.svg"/></a>`;
-    }
-    if (obj.links.youtube) {
-      content += `<a class="badge social youtube" href="${obj.links.youtube}" target="_blank" title="lien vers le YouTube"><img src="/img/social/youtube.svg"/></a>`;
-    }
-    if (obj.links.instagram) {
-      content += `<a class="badge social instagram" href="${obj.links.instagram}" target="_blank" title="lien vers le Instagram"><img src="/img/social/instagram.svg"/></a>`;
-    }
-    if (obj.links.bandcamp) {
-      content += `<a class="badge social bandcamp" href="${obj.links.bandcamp}" target="_blank" title="lien vers le bandcamp"><img src="/img/social/bandcamp.svg"/></a>`;
-    }
-
-    content += `<p>${obj.description.replace('\n', '<br>')}</p></div>
-      <button class="close-button">X</button>`;
-
-    if (obj.video) {
-      content += `<div class="fluid-video">
-        <iframe width="1280" height="720" src="${obj.video}" frameborder="0" allowfullscreen></iframe>
-      </div>`;
-    }
-
-    content += '</div>';
-    $('#artiste-content').append(content);
-  });
-
-  $('.close-button').click(e => hideModal(e.currentTarget.parentNode.id));
-}
-
-function handleModalControls() {
-  // echap ferme la modale
-  $(document).keyup((e) => { if (e.keyCode === 27) hideModal(); });
-  // click à l'extérieur ferme la modale
-  $('.modal').click(() => hideModal());
-}
-
-
-/**
  * gestion du scrolling doux
  */
 function initSmoothScroll() {
@@ -191,8 +165,7 @@ function initSmoothScroll() {
 function init() {
   initResponsiveMenu();
   initArtistes();
-  initFichesArtistes();
-  initPlanning();
+  //initPlanning();
   initMap();
   initSmoothScroll();
   handleModalControls();
