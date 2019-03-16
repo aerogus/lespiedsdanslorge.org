6ecf20c8d9225409634f8c3a81b7430a772058af
Guillaume Seznec <guillaume@seznec.fr>
2019-03-16 16:44:48 +0100

    suite artistes

--- app/artistes.js
+++ app/artistes.js
@@ -1,5 +1,24 @@
 module.exports = [
   {
+    id: 'arthedone',
+    name: 'Arthédone',
+    style: 'Pop Fr',
+    description: `
+Sur des mélodies pop chantées en français, Arthédone raconte des histoires du quotidien dans lesquelles apparaissent tour à tour un séducteur charmé ou un passionné qui tente de réaliser ses rêves quoi qu’il en coûte. Dans la ballade "Danse mon petit fou" transparait la tendresse d’un père pour son fils, à qui il faut expliquer l’absurdité du monde.
+Dans le but de tourner partout en France, des vidéos live ont été filmées lors d'une résidence à l’Avant-Scène de Rueil - Malmaison en octobre 2017. L’année 2018 fut particulièrement riche : Arthédone participe aux 46èmes rencontres d’Astaffort (P.d’honneur : Francis Cabrel ; Parrain : Christophe Maé), joue en première partie de Princess Erika et se produit à de nombreuses reprises. La sortie d'un EP est prévu en 2019.
+Des bars "underground" aux salles de spectacle, des coins du feu en solo aux concerts accompagnés de ses musiciens (Da Muss à la batterie et aux chœurs et Jules Brosset à la basse et aux chœurs), ce toulousain à Paris prend plaisir à vous en donner.
+    `,
+    photo: '/img/artistes/arthedone.jpg',
+    video: 'https://www.youtube-nocookie.com/embed/77QDFXC5p5o',
+    links: {
+      site: 'https://www.arthedone.com',
+      facebook: 'https://www.facebook.com/arthedone.music',
+      twitter: 'https://twitter.com/arthedone',
+      instagram: 'https://www.instagram.com/arthedone',
+      youtube: 'https://www.youtube.com/c/ARTHEDONE',
+    },
+  },
+  {
     id: 'conservatoire',
     name: 'Conservatoire d’Épinay-sur-Orge',
     style: 'Jazz Rock Pop',
@@ -11,20 +30,6 @@ L’atelier musiques actuelles du conservatoire proposera un set qu’il a trava
     links: {
       site: 'https://sites.google.com/site/conservatoireepinaysurorge2/',
       facebook: 'https://www.facebook.com/ConservatoireEpinaySurOrge/',
-    },
-  },
-  {
-    id: 'sozik',
-    name: 'SOZIK',
-    style: 'Jazz Rock Pop',
-    description: `
-SOZIK propose un enseignement musical inédit, axé sur la création. Venez apprendre un instrument, à composer et à enregistrer vos propres morceaux !
-    `,
-    photo: '/img/artistes/sozik.jpg',
-    video: '',
-    links: {
-      site: 'http://www.so-zik.fr/',
-      facebook: 'https://www.facebook.com/sozik91/',
     },
   },
   {
@@ -45,17 +50,20 @@ D’une composition à l’autre, humour acidulé frotte avec amour passionné, 
     },
   },
   {
-    id: 'rhymer13x7',
-    name: 'Rhymer13x7',
-    style: 'Rap Fr',
+    id: 'ecore',
+    name: 'E-Core',
+    style: 'Electro Rock',
     description: `
-Jeune rappeur de 23 ans, Rhymer a évolué depuis 6-7 dans nombre de collectifs tels que "YaH" "4Mc's" ou encore "DNA". Aujourd'hui il se concentre sur sa "carrière" solo. Son style est assez sombre mais energique. Particularité en live, il est accompagné de sa soeur comme Dj et backeuse.
+E-Core est un power-trio qui s’amuse à définir son style comme de la « dense » musique. Un son globalement rock n’roll, assorti de plein de tendances qui s’entremêlent comme un précipité.
+Leur élan naturel n’est pas de suivre le mode d’emploi à la lettre, et pas non plus de rejeter systématiquement les codes établis. Non. Ce que le groupe s’emploie à faire, c’est assembler tout ça depuis son perchoir, sans cynisme, électroniser ou punkifier la sauce, s’en approprier les ingrédients comme la recette, et servir avec délice.
+Au bout du compte, c’est comme un troupeau de vachettes, ou une charge de plastic, ou le big-bang : ça finit par nous éclater.
     `,
-    photo: '/img/artistes/rhymer13x7.jpg',
-    video: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fseriousstuffmusic%2Fvideos%2F608402309350030%2F&show_text=0&width=560&mute=0',
+    photo: '/img/artistes/ecore.jpg',
+    video: 'https://www.youtube-nocookie.com/embed/BYHQF6VM_Lo',
     links: {
-      facebook: 'https://www.facebook.com/rhymerdoctor',
-      site: 'https://fanlink.to/Rhymer13x7',
+      site: 'http://e-core.fr',
+      facebook: 'https://www.facebook.com/ecore3012/',
+      youtube: 'https://www.youtube.com/channel/UCghTqpXXpro1OHt2V-2TSyw',
     },
   },
   {
@@ -72,6 +80,59 @@ Collectif de rappeurs du 91
       soundclound: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/6455041&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false',
       facebook: 'https://www.facebook.com/PricklyPearl/',
       twitter: 'https://twitter.com/Prickly_Pearl',
+    },
+  },
+  {
+    id: 'kraken',
+    name: 'Kraken',
+    style: 'Jazz Ska, musique des Balkans',
+    description: `
+    `,
+    photo: '/img/artistes/kraken.jpg',
+    video: 'https://www.youtube-nocookie.com/embed/',
+    links: {
+      facebook: 'https://www.facebook.com/Krakengrooveband',
+    },
+  },
+  {
+    id: 'rhymer13x7',
+    name: 'Rhymer13x7',
+    style: 'Rap Fr',
+    description: `
+Jeune rappeur de 23 ans, Rhymer a évolué depuis 6-7 dans nombre de collectifs tels que "YaH" "4Mc's" ou encore "DNA". Aujourd'hui il se concentre sur sa "carrière" solo. Son style est assez sombre mais energique. Particularité en live, il est accompagné de sa soeur comme Dj et backeuse.
+    `,
+    photo: '/img/artistes/rhymer13x7.jpg',
+    video: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fseriousstuffmusic%2Fvideos%2F608402309350030%2F&show_text=0&width=560&mute=0',
+    links: {
+      facebook: 'https://www.facebook.com/rhymerdoctor',
+      site: 'https://fanlink.to/Rhymer13x7',
+    },
+  },
+  {
+    id: 'sozik',
+    name: 'SOZIK',
+    style: 'Jazz Rock Pop',
+    description: `
+SOZIK propose un enseignement musical inédit, axé sur la création. Venez apprendre un instrument, à composer et à enregistrer vos propres morceaux !
+    `,
+    photo: '/img/artistes/sozik.jpg',
+    video: '',
+    links: {
+      site: 'http://www.so-zik.fr/',
+      facebook: 'https://www.facebook.com/sozik91/',
+    },
+  },
+  {
+    id: 'salvatorium',
+    name: 'Salvatorium',
+    style: 'Metal',
+    description: `
+Influencé par le Heavy et le Thrash d’avant 2000 avec des groupes comme Iron Maiden, Metallica ou Rage Against The Machine, Salvatorium est un groupe de 4 membres. Au départ simple groupe de reprises, ils cherchent maintenant à montrer ce dont ils sont capables en composant leurs propres interprétations du genre musical qu’ils défendent.
+    `,
+    photo: '/img/artistes/salvatorium.jpg',
+    video: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F1689859227982570%2Fvideos%2F1775594402742385%2F&show_text=0&width=560&mute=0',
+    links: {
+      facebook: 'https://www.facebook.com/SalvatoriumBand',
     },
   },
   {
@@ -95,50 +156,6 @@ WHICO SKYLA c'est du rock teinté de pop, des sourires et de la sueur, des mélo
     },
   },
   {
-    id: 'salvatorium',
-    name: 'Salvatorium',
-    style: 'Metal',
-    description: `
-Influencé par le Heavy et le Thrash d’avant 2000 avec des groupes comme Iron Maiden, Metallica ou Rage Against The Machine, Salvatorium est un groupe de 4 membres. Au départ simple groupe de reprises, ils cherchent maintenant à montrer ce dont ils sont capables en composant leurs propres interprétations du genre musical qu’ils défendent.
-    `,
-    photo: '/img/artistes/salvatorium.jpg',
-    video: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F1689859227982570%2Fvideos%2F1775594402742385%2F&show_text=0&width=560&mute=0',
-    links: {
-      facebook: 'https://www.facebook.com/SalvatoriumBand',
-    },
-  },
-  {
-    id: 'ecore',
-    name: 'E-Core',
-    style: 'Electro Rock',
-    description: `
-E-Core est un power-trio qui s’amuse à définir son style comme de la « dense » musique. Un son globalement rock n’roll, assorti de plein de tendances qui s’entremêlent comme un précipité.
-Leur élan naturel n’est pas de suivre le mode d’emploi à la lettre, et pas non plus de rejeter systématiquement les codes établis. Non. Ce que le groupe s’emploie à faire, c’est assembler tout ça depuis son perchoir, sans cynisme, électroniser ou punkifier la sauce, s’en approprier les ingrédients comme la recette, et servir avec délice.
-Au bout du compte, c’est comme un troupeau de vachettes, ou une charge de plastic, ou le big-bang : ça finit par nous éclater.
-    `,
-    photo: '/img/artistes/ecore.jpg',
-    video: 'https://www.youtube-nocookie.com/embed/BYHQF6VM_Lo',
-    links: {
-      site: 'http://e-core.fr',
-      facebook: 'https://www.facebook.com/ecore3012/',
-      youtube: 'https://www.youtube.com/channel/UCghTqpXXpro1OHt2V-2TSyw',
-    },
-  },
-  {
-    id: 'kraken',
-    name: 'Kraken',
-    style: 'Jazz Ska, musique des Balkans',
-    description: `
-Marty Can Fly mélange Power-Pop et 8-Bit-Rock, et vous propose un spectacle bon-enfant qui vous plongera dans une ambiance musicale décalée, inspirée du cinéma, de la télévision et des jeux vidéo.
-Ce cocktail sonore délirant mélangeant retro et Rock-Chiptune est assurément joyeux et surprenant.
-    `,
-    photo: '/img/artistes/kraken.jpg',
-    video: 'https://www.youtube-nocookie.com/embed/MbP5cJEDHsk',
-    links: {
-      facebook: 'https://www.facebook.com/Krakengrooveband',
-    },
-  },
-  {
     id: 'zantek',
     name: 'Zantek',
     style: 'Chanson Rock',
@@ -155,22 +172,6 @@ Parmis les influences multiples, on notera Bashung, Noir Désir, Miossec, Deport
       facebook: 'https://www.facebook.com/zantekmusic',
       youtube: 'https://youtu.be/gHOXv1HApJ8',
       bandcamp: 'https://zantek.bandcamp.com/',
-    },
-  },
-  {
-    id: 'arthedone',
-    name: 'Arthédone',
-    style: 'Pop Fr',
-    description: `
-Artiste Pop Fr
-    `,
-    photo: '/img/artistes/arthedone.jpg',
-    video: 'https://www.youtube-nocookie.com/embed/77QDFXC5p5o',
-    links: {
-      facebook: 'https://www.facebook.com/arthedone.music',
-      twitter: 'https://twitter.com/arthedone',
-      instagram: 'https://www.instagram.com/arthedone',
-      youtube: 'https://www.youtube.com/c/ARTHEDONE',
     },
   },
 ];
